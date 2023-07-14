b5b9546b78afdb9184f1a4d13158befddcea7c42
Ganghyungoo <104668071+Ganghyungoo@users.noreply.github.com>
2023-07-14 18:28:34 +0900

    이게 맞나..?

--- Ganghyungoo/androidMini_MemoApp2/app/src/main/java/com/test/androidmini_memoapp2/Data.kt
+++
@@ -1,7 +0,0 @@
-package com.test.androidmini_memoapp2
-
-import java.io.Serializable
-
-data class Category(var idx:Int,var name:String):Serializable
-
-data class Memo(var idx:Int,val categoryId:Int,var memoName:String, var memoDetail:String,var date:String):Serializable
